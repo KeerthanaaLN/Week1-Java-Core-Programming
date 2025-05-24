329f060e87a0737e072c1106eb995a38d42a20c9
Keerthanaa LN <118036652+KeerthanaaLN@users.noreply.github.com>
2025-05-24 14:10:29 +0530

    Delete PracticeProblem directory

--- PracticeProblem/DistanceConverter.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class DistanceConverter{
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter the rangle in kilometers: ");
-        float kilometer = in.nextFloat();
-        System.out.println("The convereted value of kilometer into miles : "+(kilometer * 0.621371));
-    }
-}
--- PracticeProblem/TemperatureConverter.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class TemperatureConverter{
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter the temperature in Celsius : ");
-        float Celsius = in.nextFloat();
-        System.out.println("The  temperature in Fahrenheit : "+((Celsius * 9/5) + 32));
-    }
-}
